f7cb771dfc41898d447b665481c4f307970502a2
ZhangQi004 <18710471124@163.com>
2018-01-11 22:17:57 +0800

    添加关于提交给github的基础代码结构

---
+++ src/main/java/iamxb.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: iamxb
+ * Author:   Administrator
+ * Date:     2018/1/11 22:11
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2018/1/11
+ * @since 1.0.0
+ */
+public class iamxb {
+    public static void main(String[] args) {
+        System.out.println("我是张琦");
+    }
+}
